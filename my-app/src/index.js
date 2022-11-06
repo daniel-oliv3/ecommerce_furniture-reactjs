82ed853e316a70b47b02cb2364becda10a8adf0d
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-06 13:23:35 -0300

    Dependências

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "remixicon/fonts/remixicon.css";
-import "bootstrap/dist/css/bootstrap.css";
+//import "remixicon/fonts/remixicon.css";
+//import "bootstrap/dist/css/bootstrap.css";
 
 import App from "./App";
 
