2e28521dbb910c97ffa36cedcb40ed8110eebeaf
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-06 16:10:16 -0300

    Create components Header

---
+++ my-app/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <>
+            <div>Header</div>
+        </>
+    );      
+};
+
+export default Header;
+
+
+
+
+
--- my-app/src/routers/Routers.js
+++ my-app/src/routers/Routers.js
@@ -1,9 +1,25 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import Cart from "../pages/Cart";
+import ProductDetails from "../pages/ProductDetails";
+import Checkout from "../pages/Checkout";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
 
 /* ======= Routers ======= */
 const Routers = () => {
     return (
-        <div>Routers</div>
+        <Routes>
+            <Route path="home" element={<Home/>} />
+            <Route path="shop" element={<Shop/>} />
+            <Route path="Cart" element={<Cart/>} />
+            <Route path="ProductDetails" element={<ProductDetails/>} />
+            <Route path="Checkout" element={<Checkout/>} />
+            <Route path="Login" element={<Login/>} />
+            <Route path="Signup" element={<Signup/>} />
+        </Routes>
     )
 };
 
