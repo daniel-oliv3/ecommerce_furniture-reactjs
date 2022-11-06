eae953e66fe18391753172413e0bb0bdccb9113e
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-06 15:23:15 -0300

    Update

---
+++ my-app/src/pages/ProductDetails.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+/* ======= Product Details ======= */
+const ProductDetails = () => {
+    return (
+        <div>Product Details</div>
+    )
+};
+
+export default ProductDetails;
